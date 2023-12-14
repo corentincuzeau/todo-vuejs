5e72625bfe2cf032082b7b13130c38d46b4b24b7
Corentin CUZEAU <corentin.cuzeau21@gmail.com>
2023-12-14 22:16:57 +0100

    feat(app): ajout des fonctionnalités de base de la Todo

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # todo-vuejs
 Simple todo list with vue js
+
+
+## Vue JS 3
+Option utilisé (pas Composant)
---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script>
+export default {
+  data() {
+    return {
+      todoList: ["Première tache","Premier commit","Initiliasation du projet"]
+    }
+  },
+  methods:{
+    addTodo(){},
+    editTodo(){},
+    deleteTodo(){}
+  },
+  mounted(){
+    console.log("Welcome dans la todo list Vue JS 3")
+  }
+}
+</script>
+
+<template>
+  <div>
+    <h2>Todo List</h2>
+  </div>
+  <div>
+    <input name="tache" type="text">
+    <button @click="addTodo()">Ajouter</button>
+  </div>
+    <ul v-if="todoList.length>0">
+      <li v-for="todo in todoList">
+        {{ todo }}
+        <button @click="editTodo()">Modifier</button>
+        <button @click="deleteTodo()">Supprimer</button>
+      </li>
+    </ul>
+    <h4 v-else>Pas de tache(s)</h4>
+</template>
+
+<style scoped>
+</style>
